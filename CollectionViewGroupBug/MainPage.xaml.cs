e2654ed62ead9d0561cf1db7a2cf1e5ec7786f1e
simon10says <32186165+simon10says@users.noreply.github.com>
2024-02-24 01:08:42 +0800

    Add files via upload

---
+++ CollectionViewGroupBug/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CollectionViewGroupBug
+{
+    public partial class MainPage : ContentPage
+    {
+        private Animal a_bear = new() { Name = "Asian Black Bear" };
+        private Animal a_monkey = new() { Name = "Capuchin Monkey" };
+
+        public ObservableCollection<AnimalGroup> Animals { get; private set; } = [];
+
+        private Command _AddRecord;
+        public Command AddRecord
+        {
+            get
+            {
+                _AddRecord ??= new Command(() =>
+                {
+                    Animals[0].Add(a_bear);
+                });
+
+                return _AddRecord;
+            }
+        }
+
+        private Command _RemoveRecord;
+        public Command RemoveRecord
+        {
+            get
+            {
+                _RemoveRecord ??= new Command(() =>
+                {
+                    // THIS IS OK
+                    //while (Animals[0].Count > 0)
+                    //{
+                    //    Animals[0].RemoveAt(0);
+                    //}
+
+                    // EXCEPTION WHEN USE THIS
+                    Animals[0].Clear();
+                });
+
+                return _RemoveRecord;
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Animals.Add(new AnimalGroup("Bears", new List<Animal>
+            {
+                a_bear
+            }));
+
+            Animals.Add(new AnimalGroup("Monkeys", new List<Animal>
+            {
+                a_monkey
+            }));
+        }
+    }
+}
---
+++ CollectionViewGroupBug/Models.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace CollectionViewGroupBug;
+
+public class Animal
+{
+    public string Name { get; set; }
+}
+
+public class AnimalGroup : ObservableCollection<Animal>
+{
+    public string GroupName { get; private set; }
+
+    public AnimalGroup(string name, IList<Animal> animals) : base(animals)
+    {
+        GroupName = name;
+    }
+}
